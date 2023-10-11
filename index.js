bdddf6cf3dadf9c16de2b2e780ab8ee1e18fd1a4
stanleywanjau <stanleywanjau371@gmail.com>
2023-10-10 17:45:59 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution here
+function findMatching(driver,name){
+  return driver.filter(driver=> driver.toLowerCase()=== name.toLowerCase())
+}
+function fuzzyMatch(drivers,letter){
+  return drivers.filter(driver=>driver.toLowerCase().startsWith(letter.toLowerCase()))
+}
+function matchName(drivers,name){
+  return drivers.filter(driver=> driver.name === name);
+}
